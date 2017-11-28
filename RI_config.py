48d37c9f7c56663f3950cdc432548b573a1bcc10
Martina Finocchiaro <martina-finocchiaro@hotmail.it>
2017-11-28 15:10:06 +0000

    first commit

---
+++ RI_config.py
@@ -0,0 +1,17 @@
+#RI_config
+
+def riConfig(RI_num):
+    if RI_num==5:
+        maxDist=42
+        minDist=2
+        ringOffset=14
+    elif RI_num==6:
+        maxDist=40
+        minDist=2
+        ringOffset=17
+    elif RI_num==7:
+        maxDist=38
+        minDist=2
+        ringOffset=15
+    return maxDist, minDist, ringOffset
+# RI_implant
